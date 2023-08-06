b3081849b0b9096f1d753fe07bd2f06f82150f19
nurdana <nurdanarakhimova@gmail.com>
2023-08-06 23:36:23 +0500

    fix: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = (Math.abs(n)).toString();
+    let reverseNumber = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseNumber += str[i];
+    }
+  return +reverseNumber;
 }
